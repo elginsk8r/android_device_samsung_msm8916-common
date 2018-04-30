fd9f6d57070172ec9c7adbd75d8066e7cceadd10
Vinay Verma <vvinay@codeaurora.org>
2018-04-30 22:23:13 +0530

    Mount efs, firmware-modem, and firmware in vendor
    Change fstab to mount efs, firmware-modem, and firmware to /vendor/efs
    /vendor/firmware-modem, and /vendor/firmware_mnt.
    
    Add classpath for firmware loading /vendor/firmware_mnt/image in
    kernel command line.
    
    Change-Id: I2dd2e7ea6334fb979ad8ff8c755648748de93ab4

--- config.fs
+++ config.fs
@@ -201,3 +201,15 @@ mode: 0771
 user: AID_MEDIA
 group: AID_MEDIA
 caps: 0
+
+[efs/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_RADIO
+caps: 0
+
+[firmware-modem/]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
