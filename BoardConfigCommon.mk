ArbabAliRajper/device_samsung_j5nlte

msm8916-common: Add device compatibility matrix.
Add device compatibility matrix to common project and
targets can point the build variable DEVICE_MATRIX_FILE to
this file to build device compatibility matrix.

Change-Id: If8e75c5ee8de0ef50d6937ffbd6453fb3f5eb648

BoardConfigCommon.mk
@@ -197,6 +197,9 @@ TARGET_NEEDS_NETD_DIRECT_CONNECT_RULE := true
 # NFC
 BOARD_NFC_HAL_SUFFIX := msm8916
 
+# HIDL
+DEVICE_MATRIX_FILE := $(LOCAL_PATH)/compatibility_matrix.xml
+
 # Partition sizes
 BOARD_BOOTIMAGE_PARTITION_SIZE      := 13631488
 BOARD_RECOVERYIMAGE_PARTITION_SIZE  := 15728640

elginsk8r/android_device_samsung_msm8916-common

Signed-off-by: Gegham Zakaryan <zakaryan.2004@outlook.com>

BoardConfigCommon.mk
@@ -119,6 +119,7 @@ USE_DEVICE_SPECIFIC_GPS := true
 
 # HIDL
 DEVICE_MANIFEST_FILE := $(PLATFORM_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(PLATFORM_PATH)/compatibility_matrix.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := libinit_msm8916